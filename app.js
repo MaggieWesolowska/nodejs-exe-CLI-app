f8c1c945b69e99d3bd61b0b98bb17b20b84ca548
MaggieWesolowska <maggiewes07@gmail.com>
2023-07-31 21:48:11 +0200

    edited app and index to add CLI and dependencies

---
+++ app.js
@@ -0,0 +1,94 @@
+const readline = require('node:readline');
+const fs = require('node:fs');
+const { program } = require('commander');
+require('colors');
+
+program.option(
+  '-f, --file [name]',
+  'file for saving game results',
+  'results.txt'
+);
+
+program.parse(process.argv);
+
+const consoleManager = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+let count = 0;
+const logFile = program.opts().file;
+const mind = Math.floor(Math.random() * 10) + 1;
+
+const isValid = value => {
+  if (isNaN(value)) {
+    console.log('Enter number :)'.red);
+    return false;
+  }
+  if (value < 1 || value > 10) {
+    console.log(
+      'Number needs to be in range between 1 and 10'.red
+    );
+    return false;
+  }
+  return true;
+};
+
+const log = data => {
+  try {
+    fs.appendFileSync(logFile, `${data}\n`);
+    console.log(`Save to file successful ${logFile}`.green);
+  } catch (err) {
+    console.log(
+      `Save to file was not successful ${logFile}`.red
+    );
+  }
+};
+
+const game = () => {
+  consoleManager.question(
+    'Enter number between 1 and 10 to start: '.blue,
+    value => {
+      value = Number.parseInt(value, 10);
+      if (!isValid(value)) {
+        game();
+        return;
+      }
+      count += 1;
+      if (value === mind) {
+        console.log(
+          'Congratulations! You have found the correct number after %d trials. Good work!'
+            .green,
+          count
+        );
+        log(
+          `${new Date().toLocaleDateString()}: Congratulations. You have found the correct number after ${count} trials`
+        );
+        consoleManager.close();
+        return;
+      }
+      console.log('Wrong number. Try again!'.red);
+      game();
+    }
+  );
+};
+
+// const reader = readline.createInterface({
+//   input: process.stdin,
+//   output: process.stdout,
+// });
+
+// reader.on('line', input => {
+//   console.log(`Received: [${input}]\n`);
+// });
+
+consoleManager.question(
+  "Hello, what's your name?\n > ",
+  answer => {
+    consoleManager.pause();
+    consoleManager.write(
+      `Nice to meet you, ${answer}!\nLet's play a game!\n`
+    );
+    game();
+  }
+);
---
+++ index.js
@@ -0,0 +1,6 @@
+console.log('To start working with console');
+console.log("- Enter in the console: 'npm run'");
+console.log('- Choose script you want to run ex.: app.js');
+console.log(
+  "- The entire command looks like that:'npm run script_name'"
+);
